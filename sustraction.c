a9096623b52f229c464333fa64ce163faaa7cb3e
unknown <harshavardhankoganti>
2020-02-29 12:56:41 +0530

    created subtraction

--- sustraction.c
+++ sustraction.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
     int num1,num2,sum,addition;
      printf("enter the numbers");
      scanf("%d %d ",&num1,&num2);
 
-     addition=num1+num2;
+     addition=num1-num2;
      printf("%d", adiition);
 
      return 0;
-     
+    
 }
