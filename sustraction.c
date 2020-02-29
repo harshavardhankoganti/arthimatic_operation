4059f35f465dff8e76379682d52d40d5b04995aa
harshavardhankoganti <49234579+harshavardhankoganti@users.noreply.github.com>
2020-02-29 15:23:12 +0530

    Update sustraction.c

--- sustraction.c
+++ sustraction.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int num1,num3,sum,addition;
+    int num1,num2,sum,addition;
      printf("enter the numbers");
      scanf("%d %d ",&num1,&num2);
 
-     addition=num1-num3;
+     addition=num1-num2;
      printf("%d", adiition);
 
      return 0;
